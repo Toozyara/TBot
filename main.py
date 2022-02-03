1e307a7265645c5ded6afc63226a609f71a47ccf
Arseny S <toozyara@gmail.com>
2022-02-04 02:58:11 +0300

    New func from buttons

--- main.py
+++ main.py
@@ -4,44 +4,39 @@ from os import getenv
 from sys import exit
 import sqlite3
 import datetime
+import re
 
-def datanow(dataday):
-    if data
-    now = datetime.datetime.now()
-    nstr= '0' + str(100 * now.month + now.day)
-    if (len(nstr)==4):
-        nstr = nstr[0] + nstr[1] + '-' + nstr[2] + nstr[3]
+def datanow(data):
+    if datacheck(data) == True:
+        nstr = data
+        nstr = nstr[3] + nstr[4] + '-' + nstr[0] + nstr[1]
     else:
-        nstr = nstr[1] + nstr[2] + '-' + nstr[3] + nstr[4]
-    return (nstr)
+        if (data == 'now'):
+            now = datetime.datetime.now()
+        else:
+            now = datetime.datetime.now() + datetime.timedelta(days=1)
+        nstr = '0' + str(100 * now.month + now.day)
+        if (len(nstr)==4):
+            nstr = nstr[0] + nstr[1] + '-' + nstr[2] + nstr[3]
+        else:
+            nstr = nstr[1] + nstr[2] + '-' + nstr[3] + nstr[4]
 
-#print(datanow())
-
-def sql_connection():
-    con = sqlite3.connect(r'res/tangodatabase.db')
-    return con
+    return nstr
 
 sqllist = ['Дата: ', 'Время: ', 'Название: ', 'Адрес: ']
 
-def sql_table(select):
-    sql_connection().row_factory = sqlite3.Row
-    cur = sql_connection().cursor()
-    cur.execute(select)
-    return(cur.fetchall())
+def sql_man(data):
+    con = sqlite3.connect(r'res/tangodatabase.db')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("SELECT " + '*' + " FROM lessons where date = '" + datanow(data) + "' order by date desc;")
+    sqll = cur.fetchall()
+    con.cursor().close()
+    con.close()
 
-
-def locbase():
-    for sq in sqllist:
-        sqlselect = "SELECT " + '*' + " FROM lessons where date = '" + datanow() + "' order by date desc;"
-        #print(sql_table(sqlselect))
-        sqll = sql_table(sqlselect)
-        sql_connection().cursor().close()
-        sql_connection().close()
     return sqll
 
-def parsql(sqlist, ind):
-    #print(len(sqlist[0]))
-   # print(len(sqlist[0][0]))
+def parslist(sqlist, ind):
     sqlstr = ''
     #for kind in range(len(sqlist[0][0])):
      #   sqlstr = sqlstr + '\n'
@@ -49,7 +44,15 @@ def parsql(sqlist, ind):
         sqlstr = sqlstr + sqllist[kpar] + sqlist[ind][kpar] + '\n'
     return sqlstr
 
-#locbase()
+def datacheck(data):
+    match = re.search(r'\d{2}.\d{2}', data)
+    #date = datetime.strptime(match.group(), '%m-%d').date()
+    #print(date)
+    if (match != None):
+        result = True
+    else:
+        result = False
+    return result
 
 bot_token = getenv("BOT_TOKEN")
 if not bot_token:
@@ -64,6 +67,7 @@ logging.basicConfig(level=logging.INFO)
 async def send_welcome(message: types.Message):
     await message.reply("Привет!")
 """
+flaglist = [0,0,0,0,0,0]
 
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
@@ -85,17 +89,26 @@ async def send_welcome(message: types.Message):
 @dp.message_handler()
 async def echo(message: types.Message):
     if message.text == "Напишу дату":
-        await message.answer("В ожидании")
-    if message.text == "11.07":
-        await message.answer("Что-то пошло не так")
-    if message.text == "Завтра":
-        await message.answer("Что-то пошло не так")
-    if message.text == "Сегодня":
-        srts=locbase()
+        await message.answer("Укажите дату в формате: 'dd.mm'")
+        flaglist[0] = 0
+    if (datacheck(str(message.text)) == True):
+        srts = sql_man(message.text)
         for i in range(len(srts[0][0])):
-            await message.answer(parsql(locbase(), i))
+            await message.answer(parslist(srts, i))
+    else:
+        if (flaglist[0] == 1):
+            await message.answer("Что-то пошло не так")
+        flaglist[0] = 1
+    if message.text == "Завтра":
+        srts = sql_man('today')
+        for i in range(len(srts[0][0])):
+            await message.answer(parslist(srts, i))
+    if message.text == "Сегодня":
+        srts=sql_man('now')
+        for i in range(len(srts[0][0])):
+            await message.answer(parslist(srts, i))
     if message.text == "Что есть на неделе?":
-        await message.answer("Расписание готовится")
+        await message.answer("Что-то пошло не так")
     if message.text == "пидр":
         await message.answer("сам такой")
     #else:
