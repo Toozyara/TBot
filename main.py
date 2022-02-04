989bc726938c00beb3869cc30e7ad8efb6af71a1
Arseny S <toozyara@gmail.com>
2022-02-04 05:56:02 +0300

    is life

--- main.py
+++ main.py
@@ -10,19 +10,15 @@ def datanow(data):
     if datacheck(data) == True:
         nstr = data
         nstr = nstr[3] + nstr[4] + '-' + nstr[0] + nstr[1]
-    else:
-        if (data == 'now'):
-            now = datetime.datetime.now()
-        else:
-            now = datetime.datetime.now() + datetime.timedelta(days=1)
-        nstr = '0' + str(100 * now.month + now.day)
-        if (len(nstr)==4):
-            nstr = nstr[0] + nstr[1] + '-' + nstr[2] + nstr[3]
-        else:
-            nstr = nstr[1] + nstr[2] + '-' + nstr[3] + nstr[4]
+    if (data == 'now'):
+        nstr = datetime.datetime.now().strftime("%m-%d")
+    if (data == 'tomorrow'):
+        now = datetime.datetime.now() + datetime.timedelta(days=1)
+        nstr = now.strftime("%m-%d")
 
     return nstr
-
+now = datetime.datetime.now() + datetime.timedelta(days=1)
+now = now.strftime("%m-%d")
 sqllist = ['Дата: ', 'Время: ', 'Название: ', 'Адрес: ']
 
 def sql_man(data):
@@ -69,6 +65,18 @@ async def send_welcome(message: types.Message):
 """
 flaglist = [0,0,0,0,0,0]
 
+@dp.message_handler(commands=['today'])
+async def send_welcome(message: types.Message):
+    srts = sql_man('now')
+    for i in range(len(srts[0][0])):
+        await message.reply(parslist(srts, i))
+
+@dp.message_handler(commands=['tomorrow'])
+async def send_welcome(message: types.Message):
+    srts = sql_man('tomorrow')
+    for i in range(len(srts[0][0])):
+        await message.reply(parslist(srts, i))
+
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -80,7 +88,16 @@ async def cmd_start(message: types.Message):
 
 @dp.message_handler(commands=['help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Чем я могу помочь?")
+    await message.reply("У меня лапки..")
+
+@dp.message_handler(commands=['data'])
+async def send_welcome(message: types.Message):
+    await message.answer("Укажите дату в формате: 'dd.mm'")
+    flaglist[0] = 0
+    if (datacheck(str(message.text)) == True):
+        srts = sql_man(message.text)
+        for i in range(len(srts[0][0])):
+            await message.answer(parslist(srts, i))
 
 @dp.message_handler(commands=['schedule'])
 async def send_welcome(message: types.Message):
@@ -100,7 +117,7 @@ async def echo(message: types.Message):
             await message.answer("Что-то пошло не так")
         flaglist[0] = 1
     if message.text == "Завтра":
-        srts = sql_man('today')
+        srts = sql_man('tomorrow')
         for i in range(len(srts[0][0])):
             await message.answer(parslist(srts, i))
     if message.text == "Сегодня":
@@ -111,8 +128,6 @@ async def echo(message: types.Message):
         await message.answer("Что-то пошло не так")
     if message.text == "пидр":
         await message.answer("сам такой")
-    #else:
-      #  await message.answer(message.text)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
